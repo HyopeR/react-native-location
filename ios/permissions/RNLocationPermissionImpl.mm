5e525bc20bba73aca5eaa717a6fa6a6b07bde73f
Tolgahan Çelik <tol_celik@hotmail.com>
2025-11-25 20:37:32 +0300

    IOS permission instance class track AppState, always request issue fixed with tracking, but not yet completed.

--- ios/permissions/RNLocationPermissionImpl.mm
+++ ios/permissions/RNLocationPermissionImpl.mm
@@ -8,108 +8,146 @@
 @property (nonatomic, strong, readonly) CLLocationManager *manager;
 @property (nonatomic, strong) NSMutableArray<void (^)(void)> *locationHandlers;
 @property (nonatomic, strong) NSMutableArray<void (^)(void)> *locationAlwaysHandlers;
-@property (nonatomic, assign) CLAuthorizationStatus status;
+@property (nonatomic, assign) BOOL locationAlwaysRequestable;
 
 @end
 
 @implementation RNLocationPermissionImpl
 
-- (instancetype)init
-{
+- (instancetype)init {
     if (self = [super init]) {
         _manager = [[CLLocationManager alloc] init];
         _manager.delegate = self;
         _locationHandlers = [NSMutableArray array];
         _locationAlwaysHandlers = [NSMutableArray array];
-        _status = [RNLocationPermission getCurrentStatus];
+        _locationAlwaysRequestable = YES;
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(onAppWillResignActive)
+                                                     name:UIApplicationWillResignActiveNotification
+                                                   object:nil];
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(onAppDidBecomeActive)
+                                                     name:UIApplicationDidBecomeActiveNotification
+                                                   object:nil];
     }
     return self;
 }
 
-- (void)dealloc
-{
+- (void)dealloc {
     _manager.delegate = nil;
     _manager = nil;
     _locationHandlers = nil;
     _locationAlwaysHandlers = nil;
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIApplicationWillResignActiveNotification
+                                                  object:nil];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIApplicationDidBecomeActiveNotification
+                                                  object:nil];
 }
 
-- (void)checkLocation:(nonnull RCTPromiseResolveBlock)resolve reject:(nonnull RCTPromiseRejectBlock)reject
-{
+- (void)checkLocation:(nonnull RCTPromiseResolveBlock)resolve reject:(nonnull RCTPromiseRejectBlock)reject {
     @try {
-        NSString *status = [RNLocationPermission checkLocation];
+        NSString *status = [RNLocationPermission toJs:[RNLocationPermission checkLocation]];
         resolve(status);
     } @catch (NSException *e) {
         [RNLocationUtils handleException: e resolve: resolve reject: reject];
     }
 }
 
-- (void)checkLocationAlways:(nonnull RCTPromiseResolveBlock)resolve reject:(nonnull RCTPromiseRejectBlock)reject
-{
+- (void)checkLocationAlways:(nonnull RCTPromiseResolveBlock)resolve reject:(nonnull RCTPromiseRejectBlock)reject {
     @try {
-        NSString *status = [RNLocationPermission checkLocationAlways];
+        NSString *status = [RNLocationPermission toJs:[RNLocationPermission checkLocationAlways]];
         resolve(status);
     } @catch (NSException *e) {
         [RNLocationUtils handleException: e resolve: resolve reject: reject];
     }
 }
 
-- (void)requestLocation:(nonnull RCTPromiseResolveBlock)resolve reject:(nonnull RCTPromiseRejectBlock)reject
-{
+- (void)requestLocation:(nonnull RCTPromiseResolveBlock)resolve reject:(nonnull RCTPromiseRejectBlock)reject {
     @try {
         NSString *status = [RNLocationPermission checkLocation];
+        NSString *statusJs = [RNLocationPermission toJs:status];
         if (status != RNLocationPermissionStatus.DENIED) {
-            resolve(status);
+            resolve(statusJs);
             return;
         }
         
-        [self.locationHandlers addObject:^{
-            NSString *status = [RNLocationPermission checkLocation];
-            resolve(status);
-        }];
+        void (^callback)(void) = ^{
+            NSString *callbackStatus = [RNLocationPermission checkLocation];
+            NSString *callbackStatusJs = [RNLocationPermission toJs:callbackStatus];
+            resolve(callbackStatusJs);
+        };
+        [self.locationHandlers addObject:callback];
         [self.manager requestWhenInUseAuthorization];
     } @catch (NSException *e) {
         [RNLocationUtils handleException: e resolve: resolve reject: reject];
     }
 }
 
-- (void)requestLocationAlways:(nonnull RCTPromiseResolveBlock)resolve reject:(nonnull RCTPromiseRejectBlock)reject
-{
+- (void)requestLocationAlways:(nonnull RCTPromiseResolveBlock)resolve reject:(nonnull RCTPromiseRejectBlock)reject {
     @try {
         NSString *status = [RNLocationPermission checkLocationAlways];
-        if (status != RNLocationPermissionStatus.DENIED) {
-            resolve(status);
+        NSString *statusJs = [RNLocationPermission toJs:status];
+        if (status != RNLocationPermissionStatus.UPGRADEABLE) {
+            resolve(statusJs);
             return;
         }
 
-        [self.locationAlwaysHandlers addObject:^{
-            NSString *status = [RNLocationPermission checkLocationAlways];
-            resolve(status);
-        }];
+        void (^callback)(void) = ^{
+            NSString *callbackStatus = [RNLocationPermission checkLocationAlways];
+            NSString *callbackStatusJs = callbackStatus == RNLocationPermissionStatus.GRANTED
+                ? RNLocationPermissionStatus.GRANTED
+                : RNLocationPermissionStatus.BLOCKED;
+            resolve(callbackStatusJs);
+        };
+        [self.locationAlwaysHandlers addObject: callback];
+
+        self.locationAlwaysRequestable = YES;
+        int64_t delta = (int64_t)(0.25 * NSEC_PER_SEC);
+        dispatch_queue_t queue = dispatch_get_main_queue();
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delta), queue, ^{
+            [self onApplicationWillResignActiveCheck];
+        });
+        
         [self.manager requestAlwaysAuthorization];
     } @catch (NSException *e) {
         [RNLocationUtils handleException: e resolve: resolve reject: reject];
     }
 }
 
-#pragma mark - CLLocationManagerDelegate
+- (void)resolveHandlers:(NSMutableArray<void (^)(void)> *)handlers {
+    if (handlers.count == 0) return;
 
-- (void)locationManagerDidChangeAuthorization:(CLLocationManager *)manager {
-    self.status = [RNLocationPermission getCurrentStatus];
-    
-    if (self.locationHandlers.count > 0) {
-        for (void (^callback)(void) in self.locationHandlers) {
-            callback();
-        }
-        [self.locationHandlers removeAllObjects];
+    for (void (^callback)(void) in handlers) {
+        callback();
     }
-    
+
+    [handlers removeAllObjects];
+}
+
+- (void)onApplicationWillResignActiveCheck {
+    if (!self.locationAlwaysRequestable || self.locationAlwaysHandlers.count == 0) return;
+    [self resolveHandlers:self.locationAlwaysHandlers];
+}
+
+- (void)onAppWillResignActive {
+    // Triggered when the application is inactive.
+    if (self.locationHandlers.count == 0 && self.locationAlwaysHandlers.count == 0) return;
     if (self.locationAlwaysHandlers.count > 0) {
-        for (void (^callback)(void) in self.locationAlwaysHandlers) {
-            callback();
-        }
-        [self.locationAlwaysHandlers removeAllObjects];
+        self.locationAlwaysRequestable = NO;
     }
+}
+
+- (void)onAppDidBecomeActive {
+    // Triggered when the application is active.
+    if (self.locationHandlers.count == 0 && self.locationAlwaysHandlers.count == 0) return;
+    [self resolveHandlers:self.locationHandlers];
+    [self resolveHandlers:self.locationAlwaysHandlers];
 }
 
 @end
