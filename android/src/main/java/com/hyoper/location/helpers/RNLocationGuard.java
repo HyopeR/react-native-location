f51e1e9427f7db8baea4d48ef2420b3840ef3aed
Tolgahan Çelik <tol_celik@hotmail.com>
2025-11-27 17:16:01 +0300

    Android guard class revised similar to iOS logic.

--- android/src/main/java/com/hyoper/location/helpers/RNLocationGuard.java
+++ android/src/main/java/com/hyoper/location/helpers/RNLocationGuard.java
@@ -39,20 +39,20 @@ public final class RNLocationGuard {
         try {
             List<String> missing = new ArrayList<>();
 
-            if (!hasPermissionInManifest(context, COARSE_LOCATION)) {
+            PackageInfo info = getPermissionInfo(context);
+
+            if (!hasPermission(COARSE_LOCATION, info)) {
                 missing.add(COARSE_LOCATION);
             }
-
-            if (!hasPermissionInManifest(context, FINE_LOCATION)) {
+            if (!hasPermission(FINE_LOCATION, info)) {
                 missing.add(FINE_LOCATION);
             }
 
             if (!missing.isEmpty()) {
-                throw new RNLocationException(Error.SETUP, "Setup missing: " + String.join(", ", missing), true);
+                throwException(missing);
             }
-        } catch (PackageManager.NameNotFoundException e) {
-            String message = (e.getMessage() != null) ? e.getMessage() : "Unknown error.";
-            throw new RNLocationException(Error.SETUP, "Setup runtime issue: " + message, true);
+        } catch (Exception e) {
+            throwHandleException(e);
         }
     }
 
@@ -62,66 +62,72 @@ public final class RNLocationGuard {
 
             List<String> missing = new ArrayList<>();
 
+            PackageInfo info = getPermissionInfo(context);
+            PackageInfo infoService = getServicesInfo(context);
+
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                if (!hasPermissionInManifest(context, BACKGROUND_LOCATION)) {
+                if (!hasPermission(BACKGROUND_LOCATION, info)) {
                     missing.add(BACKGROUND_LOCATION);
                 }
-                if (!hasServiceInManifest(context, FOREGROUND_SERVICE_NAME)) {
+                if (!hasService(FOREGROUND_SERVICE_NAME, infoService)) {
                     missing.add(FOREGROUND_SERVICE_NAME);
                 }
-                if (!hasPermissionInManifest(context, FOREGROUND_SERVICE)) {
+                if (!hasPermission(FOREGROUND_SERVICE, info)) {
                     missing.add(FOREGROUND_SERVICE);
                 }
             }
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-                if (!hasPermissionInManifest(context, FOREGROUND_SERVICE_LOCATION)) {
+                if (!hasPermission(FOREGROUND_SERVICE_LOCATION, info)) {
                     missing.add(FOREGROUND_SERVICE_LOCATION);
                 }
             }
 
             if (!missing.isEmpty()) {
-                throw new RNLocationException(Error.SETUP, "Setup missing: " + String.join(", ", missing), true);
+                throwException(missing);
             }
         } catch (PackageManager.NameNotFoundException e) {
-            String message = (e.getMessage() != null) ? e.getMessage() : "Unknown error.";
-            throw new RNLocationException(Error.SETUP, "Setup runtime issue: " + message, true);
+            throwHandleException(e);
         }
     }
 
-    private static boolean hasPermissionInManifest(@NonNull Context context, @NonNull String permission) throws PackageManager.NameNotFoundException {
-        PackageManager pm = context.getPackageManager();
-        PackageInfo info = pm.getPackageInfo(context.getPackageName(), PackageManager.GET_PERMISSIONS);
+    private static PackageInfo getPermissionInfo(@NonNull Context context) throws PackageManager.NameNotFoundException {
+        return context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_PERMISSIONS);
+    }
 
-        if (info == null || info.requestedPermissions == null) {
-            return false;
-        }
+    private static PackageInfo getServicesInfo(@NonNull Context context) throws PackageManager.NameNotFoundException {
+        return context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_SERVICES);
+    }
 
+    private static boolean hasPermission(@NonNull String permission, @NonNull PackageInfo info) {
+        if (info.requestedPermissions == null) return false;
         for (String p : info.requestedPermissions) {
-            if (permission.equals(p)) {
-                return true;
-            }
+            if (permission.equals(p)) return true;
         }
-
         return false;
     }
 
-    private static boolean hasServiceInManifest(@NonNull Context context, @NonNull String name) throws PackageManager.NameNotFoundException {
-        PackageManager pm = context.getPackageManager();
-        PackageInfo info = pm.getPackageInfo(context.getPackageName(), PackageManager.GET_SERVICES);
-
-        if (info == null || info.services == null) {
-            return false;
-        }
-
+    private static boolean hasService(@NonNull String name, @NonNull PackageInfo info) {
+        if (info.services == null) return false;
         for (ServiceInfo s : info.services) {
             if (s == null || s.name == null) continue;
-            if (s.name.endsWith(name)) {
-                return true;
-            }
+            if (s.name.endsWith(name)) return true;
         }
-
         return false;
+    }
+
+    private static void throwException(@NonNull List<String> array) throws RNLocationException {
+        String message = String.join(", ", array);
+        throw new RNLocationException(Error.SETUP, "Setup missing: " + message, true);
+    }
+
+    private static void throwHandleException(@NonNull Exception exception) throws RNLocationException {
+        if (exception instanceof RNLocationException) {
+            throw (RNLocationException) exception;
+        } else {
+            String message = (exception.getMessage() != null) ? exception.getMessage() : "Unknown error.";
+            throw new RNLocationException(Error.SETUP, "Setup runtime issue: " + message, true);
+        }
     }
 
     public static boolean hasFusedLocationProvider() {
