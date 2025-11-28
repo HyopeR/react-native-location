36e1e7de88b227dcbe55a0ae7500716ef9501e6b
Tolgahan Çelik <tol_celik@hotmail.com>
2025-11-28 17:14:12 +0300

    IOS manager instance class added.

---
+++ ios/manager/RNLocationManagerImpl.mm
@@ -0,0 +1,31 @@
+#import "RNLocationManagerImpl.h"
+#import "RNLocationManager.h"
+#import "RNLocationConstants.h"
+#import "RNLocationException.h"
+
+@implementation RNLocationManagerImpl
+
+- (instancetype)init {
+    if (self = [super init]) {}
+    return self;
+}
+
+- (void)checkGps:(nonnull RCTPromiseResolveBlock)resolve
+          reject:(nonnull RCTPromiseRejectBlock)reject {
+    @try {
+        [RNLocationManager ensure:YES];
+        resolve(@(YES));
+    } @catch (NSException *e) {
+        resolve(@(NO));
+    }
+}
+
+- (void)openGps:(nonnull RCTPromiseResolveBlock)resolve
+         reject:(nonnull RCTPromiseRejectBlock)reject {
+    @throw [[RNLocationException alloc]
+            initWithCode:RNLocationError.UNKNOWN
+            message:@"This feature is not supported on IOS."
+            critical:NO];
+}
+
+@end
